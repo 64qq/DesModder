a2b29c4133362afeeb18021aab1d15d5cecdb2b2
64 <172265251+64qq@users.noreply.github.com>
2025-04-22 18:54:15 +0900

    Annotate the argument `handler` of `setIntellisenseTimeout` as `() => void`

--- src/plugins/intellisense/utils.ts
+++ src/plugins/intellisense/utils.ts
@@ -1,9 +1,9 @@
 let intellisenseTimeoutID = 0;
 export const pendingIntellisenseTimeouts = new Set();
 export const setIntellisenseTimeout = function (
-  handler: () => any,
+  handler: () => void,
   timeout?: number
-): any {
+) {
   const thisInvocationID = intellisenseTimeoutID++;
   pendingIntellisenseTimeouts.add(thisInvocationID);
   // eslint-disable-next-line @desmodder/eslint-rules/no-timeouts-in-intellisense
