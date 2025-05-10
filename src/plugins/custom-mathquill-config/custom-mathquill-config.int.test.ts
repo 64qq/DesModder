1ff615379d0323ab078aafa8a48955cc48e5aff2
64 <172265251+64qq@users.noreply.github.com>
2025-05-10 15:41:05 +0900

    Don't access to states directly and assert with `assertSelectedItemLatex` instead in custom-mathquill-config tests

--- src/plugins/custom-mathquill-config/custom-mathquill-config.int.test.ts
+++ src/plugins/custom-mathquill-config/custom-mathquill-config.int.test.ts
@@ -1,76 +1,7 @@
 import { clean, testWithPage } from "#tests";
 
-const STATE_BASE = {
-  version: 10,
-  randomSeed: "2526a837de1b19af17098be805151859",
-  graph: {
-    viewport: {
-      xmin: -10,
-      ymin: -14.724969097651421,
-      xmax: 10,
-      ymax: 14.724969097651421,
-    },
-  },
-  expressions: {
-    list: [
-      {
-        type: "expression",
-        id: "1",
-        color: "#c74440",
-      },
-    ],
-  },
-};
-
-const STATE_GREEK = {
-  version: 10,
-  randomSeed: "2526a837de1b19af17098be805151859",
-  graph: {
-    viewport: {
-      xmin: -10,
-      ymin: -14.724969097651421,
-      xmax: 10,
-      ymax: 14.724969097651421,
-    },
-  },
-  expressions: {
-    list: [
-      {
-        type: "expression",
-        id: "1",
-        color: "#c74440",
-        latex: "\\omega",
-      },
-    ],
-  },
-};
-
-const STATE_SUPERSCRIPT = {
-  version: 10,
-  randomSeed: "2526a837de1b19af17098be805151859",
-  graph: {
-    viewport: {
-      xmin: -10,
-      ymin: -14.724969097651421,
-      xmax: 10,
-      ymax: 14.724969097651421,
-    },
-  },
-  expressions: {
-    list: [
-      {
-        type: "expression",
-        id: "1",
-        color: "#c74440",
-        latex: "e^{2+2}",
-      },
-    ],
-  },
-};
-
 testWithPage("More Greek Letters", async (driver) => {
   await driver.enablePlugin("custom-mathquill-config");
-  await driver.setState(STATE_BASE as any);
   await driver.focusIndex(0);
   await driver.setPluginSetting(
     "custom-mathquill-config",
@@ -78,7 +9,7 @@ testWithPage("More Greek Letters", async (driver) => {
     true
   );
   await driver.keyboard.type("omega");
-  await driver.assertExprsList(STATE_GREEK as any);
+  await driver.assertSelectedItemLatex("\\omega");
 
   await driver.clean();
   return clean;
@@ -86,7 +17,6 @@ testWithPage("More Greek Letters", async (driver) => {
 
 testWithPage("Operators in Exponents", async (driver) => {
   await driver.enablePlugin("custom-mathquill-config");
-  await driver.setState(STATE_BASE as any);
   await driver.focusIndex(0);
   await driver.setPluginSetting(
     "custom-mathquill-config",
@@ -94,7 +24,7 @@ testWithPage("Operators in Exponents", async (driver) => {
     true
   );
   await driver.keyboard.type("e^2+2");
-  await driver.assertExprsList(STATE_SUPERSCRIPT as any);
+  await driver.assertSelectedItemLatex("e^{2+2}");
 
   await driver.clean();
   return clean;
