a8e67fc25a61b5b9d72030c207005caa5ad4ac1a
64 <172265251+64qq@users.noreply.github.com>
2025-05-19 01:17:18 +0900

    Fix `migrateToLatest` and `migrateToLatestMaybe` to accept a value of type unknown

--- metadata/migrate.ts
+++ metadata/migrate.ts
@@ -1,26 +1,36 @@
-import Metadata from "./interface";
+import Metadata, { MetadataV1 } from "./interface";
 import { getBlankMetadata } from "./manage";
 import migrate1to2 from "./migrations/migrate1to2";
 
-export default function migrateToLatest(metadata: any): Metadata {
+const isStringArray = (value: unknown): value is string[] =>
+  Array.isArray(value) && value.every((el) => typeof el === "string");
+
+/**
+ * Discriminate version 1 by using the presence of the pinnedExpressions
+ *  property (it was the only property)
+ */
+const isMetadataV1 = (metadata: object): metadata is MetadataV1 =>
+  "pinnedExpressions" in metadata && isStringArray(metadata.pinnedExpressions);
+
+// might want schema validation (it doesn't check the validity of `metadata.expressions`)
+const isMetadataV2 = (metadata: object): metadata is Metadata =>
+  "version" in metadata &&
+  "expressions" in metadata &&
+  metadata.version === 2 &&
+  typeof metadata.expressions === "object";
+
+export default function migrateToLatest(metadata: unknown): Metadata {
   return migrateToLatestMaybe(metadata) ?? getBlankMetadata();
 }
 
-export function migrateToLatestMaybe(metadata: any): Metadata | undefined {
-  if ("pinnedExpressions" in metadata) {
-    /* Discriminate version 1 by using the presence of the pinnedExpressions
-    property (it was the only property) */
-    metadata = migrate1to2(metadata);
-  }
-  if (metadata.version !== 2) {
+export function migrateToLatestMaybe(metadata: unknown): Metadata | undefined {
+  if (typeof metadata !== "object" || metadata === null) return undefined;
+  const metadataV2Maybe = isMetadataV1(metadata)
+    ? migrate1to2(metadata)
+    : metadata;
+  if (!isMetadataV2(metadataV2Maybe)) {
     // Something went wrong with migration. Just return a blank metadata
     return undefined;
   }
-  if (
-    !("expressions" in metadata) ||
-    typeof metadata.expressions !== "object"
-  ) {
-    return undefined;
-  }
-  return metadata;
+  return metadataV2Maybe;
 }
