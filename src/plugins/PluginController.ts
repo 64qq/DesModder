049f1dd104e0b3ca8450223f37bf44e9bdd42554
64 <172265251+64qq@users.noreply.github.com>
2025-05-25 21:19:16 +0900

    Use specific plugin instance types in `enabledPlugins`

--- src/plugins/PluginController.ts
+++ src/plugins/PluginController.ts
@@ -1,9 +1,13 @@
 import { ConfigItem, OptionalGenericSettings } from "./config";
 import DSM from "#DSM";
+import { PluginInstance } from ".";
+import { ActionButton } from "../core-plugins/expr-action-buttons";
+import { DispatchedEvent } from "../globals/extra-actions";
 
 export class PluginController<
   Settings extends OptionalGenericSettings<Settings> = undefined,
-> {
+> implements PluginInstance<Settings>
+{
   static descriptionLearnMore?: string = undefined;
   static forceEnabled?: boolean = undefined;
   static config: readonly ConfigItem[] | undefined;
@@ -21,6 +25,17 @@ export class PluginController<
   afterConfigChange() {}
   beforeDisable() {}
   afterDisable() {}
+
+  /** Consumed by expr-action-buttons. This should really be a facet a la Codemirror. */
+  actionButtons?: ActionButton[];
+
+  /** Returning `"abort-later-handlers"` means don't run any later handlers. */
+  handleDispatchedAction?(
+    evt: DispatchedEvent
+  ): "abort-later-handlers" | undefined;
+  afterHandleDispatchedAction?(evt: DispatchedEvent): void;
+  beforeUpdateTheComputedWorld?(): void;
+  afterUpdateTheComputedWorld?(): void;
 }
 
 export type Replacer<T = any> = undefined | ((old: T) => any);
