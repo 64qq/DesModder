e7c0efabd7214f1031307ebc8ea8a5469037161b
64 <172265251+64qq@users.noreply.github.com>
2025-05-28 22:24:44 +0900

    Don't use `any` when assigning `dsm_compileGLesmos`

--- src/preload/replacementHelpers/append.inline.ts
+++ src/preload/replacementHelpers/append.inline.ts
@@ -1,5 +1,9 @@
 import { compileGLesmos } from "#plugins/GLesmos/exportAsGLesmos.ts";
 
-(self as any).dsm_compileGLesmos = compileGLesmos as any;
+// WorkerGlobalScope?
+declare const self: typeof globalThis & {
+  dsm_compileGLesmos: typeof compileGLesmos;
+};
+self.dsm_compileGLesmos = compileGLesmos;
 
 export default "";
