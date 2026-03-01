0aa5b9b157f3109b8c408420916686eb0786e038
64 <172265251+64qq@users.noreply.github.com>
2026-03-02 00:10:30 +0900

    Remove duplication in label helper definitions

--- src/plugins/better-evaluation-view/label.ts
+++ src/plugins/better-evaluation-view/label.ts
@@ -9,20 +9,22 @@ const labelOptions = {
 
 const { Label } = Private.Mathtools;
 
-type ComplexNumberLabel = typeof Label.complexNumberLabel;
-type PointLabel = typeof Label.pointLabel;
-type Point3dLabel = typeof Label.point3dLabel;
-type TruncatedLatexLabel = typeof Label.truncatedLatexLabel;
+const withUprightUndefined =
+  <T, R extends unknown[]>(
+    format: (label: T, labelOptions: LabelOptionsBase, ...rest: R) => string,
+    opts?: LabelOptionsBase,
+    ...rest: R
+  ) =>
+  (label: T) => {
+    const formatted = format(label, opts ?? labelOptions, ...rest);
+    return formatted === "undefined" ? "\\mathrm{undefined}" : formatted;
+  };
 
-const uprightUndefined = (label: string) =>
-  label === "undefined" ? "\\mathrm{undefined}" : label;
-
-export const complexNumberLabel = (label: Parameters<ComplexNumberLabel>[0]) =>
-  uprightUndefined(Label.complexNumberLabel(label, labelOptions));
-export const pointLabel = (label: Parameters<PointLabel>[0]) =>
-  uprightUndefined(Label.pointLabel(label, labelOptions));
-export const point3dLabel = (label: Parameters<Point3dLabel>[0]) =>
-  uprightUndefined(Label.point3dLabel(label, labelOptions));
-export const truncatedLatexLabel = (
-  label: Parameters<TruncatedLatexLabel>[0]
-) => uprightUndefined(Label.truncatedLatexLabel(label, labelOptions));
+export const complexNumberLabel = withUprightUndefined(
+  Label.complexNumberLabel
+);
+export const pointLabel = withUprightUndefined(Label.pointLabel);
+export const point3dLabel = withUprightUndefined(Label.point3dLabel);
+export const truncatedLatexLabel = withUprightUndefined(
+  Label.truncatedLatexLabel
+);
